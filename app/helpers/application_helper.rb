911d8983f612a3d3974a53ed902116f49ffb792d
Yahya Islamovic <yahya.islamovic.work@live.com>
2021-09-15 21:25:23 -0400

    added helper function that displays html when the cart is empty; conditionally display the cart if it contains something

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def is_cart_empty?
+    html = ''
+    if enhanced_cart.empty?
+      html << "<p>... Is Empty! Please Buy Things @ 
+      <a href='/'> Home Page</a></p>"
+    end  
+    html
+  end
 end
