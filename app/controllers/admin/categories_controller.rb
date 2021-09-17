f89e580f17d8f59178fc725ce8efa097a95dcc0c
Yahya Islamovic <yahya.islamovic.work@live.com>
2021-09-17 17:00:36 -0400

    created controller and view for adding new categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -6,11 +6,15 @@ class Admin::CategoriesController < ApplicationController
     @catagory = Category.new
   end 
   def create
-    @catagory = Category.new(params[:name])
+    @catagory = Category.new(catagory_params)
     if @catagory.save
       redirect_to [:admin, :categories], notice: 'Category created!'
     else
       render :new
     end
   end  
+  private
+  def catagory_params
+    params.require(:category).permit(:name)
+  end
 end
