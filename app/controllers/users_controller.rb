8966a57f6bf1c7dea953c55c48b6e6ae93434802
Yahya Islamovic <yahya.islamovic.work@live.com>
2021-09-22 17:37:24 -0400

    final changes; bug fixes and feature tweaks after looking at project description

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,6 @@ class UsersController < ApplicationController
   end   
   private
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:email, :password, :password_confirmation, :first_name, :last_name)
   end
 end
--- db/migrate/20210917233900_create_users.rb
+++ db/migrate/20210917233900_create_users.rb
@@ -3,7 +3,8 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :email
       t.string :password_digest
-
+      t.string :first_name
+      t.string :last_name
       t.timestamps null: false
     end
   end
